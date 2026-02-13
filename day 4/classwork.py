13ead1f8d808effdb47e0c2bf3d2b9186ff78e44
beka293 <b25827048@gmail.com>
2026-02-13 18:14:16 +0300

    day 4

---
+++ day 4/classwork.py
@@ -0,0 +1,8 @@
+user_name = "beka"
+last_name = "meskhia"
+user_age = "17" 
+user_adress = "apkhazia"
+user_hobby = "i don't have"
+
+print(user_name +" "+ last_name +" "+ user_age +" "+ user_adress +" "+ user_hobby)
+ 
---
+++ day 4/homework.py
@@ -0,0 +1,29 @@
+# input ნიშნავს მონაცემთა შეყვანას პროგრამაში მოხმარებლისგან ან სხვა წყაროდან ხოლო output-შედეგის გამოტანას ეკრანზე ან ფაილში
+
+user_name = input("Enter Your name: ")
+print(user_name)
+
+last_name = input("Enter Your last name: ")
+print(last_name)
+
+
+user_age = input("Enter Your age: ")
+print(user_age)
+
+user_adress = input("Enter Your adress: ")
+print(user_adress)
+
+user_gender = input("Enter Your gender: ")
+print(user_gender)
+
+
+# input მოწყობილებები:კლავიატურა,მაუსი,მიკროფონი,ვებკამერა,სკანერი.
+# output მოწყობილებები:მონიტორი,პრინტრი,სპიკერები,ყურსასმენი.
+
+
+
+# დავალება 2
+
+User_name = input("your name: ")
+print(User_name)
+
