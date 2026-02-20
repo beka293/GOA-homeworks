a4976f547edb5e82184de81e9130c432e20feff7
beka293 <b25827048@gmail.com>
2026-02-20 16:42:03 +0300

    day 6

---
+++ day 6/classwork.py
@@ -0,0 +1,16 @@
+dog = input("შეიყვანე შენი ძაღლის სახელი: ")
+
+print("ძაღლის სახელია:", dog)
+
+print("dog ცვლადის ტიპი:", type(dog))
+
+
+
+#birth_year = int(input("შეიყვანე შენი დაბადების წელი: "))
+
+#current_year = 2026
+
+#age = current_year - birth_year
+
+
+#print("შენი ასაკი არის:", age, "წელი")
