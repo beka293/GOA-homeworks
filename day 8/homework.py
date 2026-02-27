d6bd81a283a54cbe8c4503bccd3e3f76807fc336
beka293 <b25827048@gmail.com>
2026-02-27 16:29:27 +0300

    day 8

---
+++ day 8/homework.py
@@ -0,0 +1,66 @@
+# int ცვლადები
+age = 25
+count = 10
+year = 2026
+score = 100
+day = 27
+
+# float ცვლადები
+height = 1.75
+price = 19.99
+temperature = 22.5
+distance = 5.5
+bmi = 24.3
+
+# str ცვლადები
+name = "Beka"
+city = "აფხაზეთი"
+message = "გამარჯობა"
+code = "Python"
+surname = "Meskhia"
+
+# bool ცვლადები
+is_adult = True
+is_valid = False
+has_access = True
+is_even = False
+finished = True
+
+#ოპერატორი არის სიმბოლო ან საკვანძო სიტყვა, რომელიც ოპერაციას ასრულებს ობიექტებს შორის, მაგალითად მათემატიკური გამოთვლა ან შედარება.
+#რითმეტიკული: 5 + 3 = 8
+#შედარების: 10 > 5 → True
+#ლოგიკური: True and False → False
+#მინიჭების: x = 10
+
+#მეტი
+print(15 > 10)    # True
+print(5 > 8)      # False  
+print(20.5 > 20)  # True
+print(-1 > 0)     # False
+print("b" > "a")  # True
+
+#ნაკლები
+print(7 < 12)     # True
+print(10 < 5)     # False
+print(9.9 < 10)   # True
+print(0 < -3)     # False
+print("a" < "b")  # True
+
+#მეტი ან ტოლი
+print(10 >= 10)   # True
+print(8 >= 12)    # False
+print(15.0 >= 15) # True
+print(5 >= 7)     # False
+print("z" >= "a") # True
+
+#ნაკლები ან ტოლი
+print(5 <= 5)     # True
+print(12 <= 8)    # False
+print(9.5 <= 10)  # True
+print(20 <= 15)   # False
+print("a" <= "b") # True
+
+
+#Boolean არის მონაცემთა ტიპი, რომელიც გამოხატავს შეჯერებულ მნიშვნელობას: True (ჭეშმარიტი) ან False (მცდარი).
+
+
