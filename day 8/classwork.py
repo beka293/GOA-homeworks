a367b10c57f385969b833e4985703f8838fcb304
beka293 <b25827048@gmail.com>
2026-02-27 16:24:21 +0300

    day 8

---
+++ day 8/classwork.py
@@ -0,0 +1,4 @@
+age_input = input("შეყვანეთ თქვენი ასაკი: ") 
+age = int(age_input)
+age >= 18
+print(age_input)
---
+++ day 8/hw/homework2.py
@@ -0,0 +1,24 @@
+Num1 = float(input("შეიტანეთ პირველი რიცხვი: "))
+Num2 = float(input("შეიტანეთ მეორე რიცხვი: "))
+
+print(Num1>=Num2)
+
+
+
+
+nuM1 = float(input("შეიტანეთ პირველი რიცხვი: "))
+nuM2 = float(input("შეიტანეთ მეორე რიცხვი: "))
+
+
+print(nuM1>=10)
+print(nuM2>=20)
+
+
+number = int(input("შეიტანეთ რიცხვი: "))
+ 
+num =  100 <= number <= 999
+
+print(num)
+
+
+
