ddfaa1f34087e1741a5d4b8a36207027321ded53
beka293 <b25827048@gmail.com>
2026-03-31 19:55:51 +0300

    day 16

---
+++ day 16/cw.py
@@ -0,0 +1,41 @@
+from turtle import *
+
+speed(5)
+
+
+penup()
+goto(-150, 100)
+pendown()
+color("blue")
+for i in range(4):
+    forward(80)
+    right(90)
+
+
+penup()
+goto(-20, 100)
+pendown()
+color("red")
+for i in range(4):
+    forward(80)
+    right(90)
+
+
+penup()
+goto(-150, 0)
+pendown()
+color("green")
+for i in range(4):
+    forward(80)
+    right(90)
+
+
+penup()
+goto(-20, 0)
+pendown()
+color("orange")
+for i in range(4):
+    forward(80)
+    right(90)
+
+
