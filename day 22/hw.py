dc6ea581568e44f27bc8a1ce910c5439d1d169fd
beka293 <b25827048@gmail.com>
2026-04-17 20:31:10 +0300

    day 22

---
+++ day 22/hw.py
@@ -0,0 +1,59 @@
+# 1
+name = "Beka"
+print(name[0])
+
+# 2
+surname = "Meskhia"
+print(surname[-1])
+
+# 3
+s = "Python"
+print(s[2])
+
+# 4
+g = "Programming"
+print(g[:4])
+
+# 5
+p = "Computer"
+print(p[-3:])
+
+# 6
+h = "HelloWorld"
+print(h[3:7])
+
+# 7
+ge = "Georgia"
+print(ge[2:6])
+
+# 8
+word = "Apple"
+if word[0] == "A":
+    print(word[:3])
+else:
+    print(word[-3:])
+
+# 9
+d = "Education"
+print(d[1:-1])
+
+# 10
+s = "Developer"
+new_s = s[:3] + s[-3:]
+print(new_s)
+
+# 11
+sentence = "Python is very useful for learning programming"
+print(sentence[:sentence.find(" ")])
+
+# 12
+s = "abcdef"
+print(s[::2])
+
+# 13
+lst = [10, "two", 3.5, True, "five", 6, 7, 8]
+print(lst[2:6])
+
+# 14
+musicians = ["Bach", "Mozart", "Beethoven", "Chopin", "Brahms", "Vivaldi"]
+print(musicians[-2:])
