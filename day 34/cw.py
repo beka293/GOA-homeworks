58ec37e1f607e83ac5be9de7aee6f5be0f744750
beka293 <b25827048@gmail.com>
2026-06-04 17:43:19 +0300

    day 34

---
+++ day 34/cw.py
@@ -0,0 +1,14 @@
+# def mn():
+#     print('beka meskhia')
+
+# mn()
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        print("რიცხვი ლუწია")
+    else:
+        print("რიცხვი კენტია")
+
+
+check_even_odd(143)  
+check_even_odd(19)   
